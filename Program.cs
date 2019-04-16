896c2ea0a2ab26aa7d75fef711d11a3c284c1d1c
StephanieK94 <49422706+StephanieK94@users.noreply.github.com>
2019-04-16 14:19:54 +1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 100;
+
+            for(int i=1; i<=range; i++)
+            {
+                if (i % 3==0 || i%5==0)
+                {
+                    if(i % 3 == 0)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    if(i % 5 == 0)
+                    {
+                        Console.Write("Buzz");
+                    }
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
